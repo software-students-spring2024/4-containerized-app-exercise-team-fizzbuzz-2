0063edcc1aba56f7f344305b2a9ea6b2a0b43872
FirasBDarwish <darwish.firas.b@gmail.com>
2024-04-07 00:46:17 -0400

    reformatted with black:

--- machine-learning-client/inference.py
+++ machine-learning-client/inference.py
@@ -10,14 +10,15 @@ from transformers import Speech2TextProcessor, Speech2TextForConditionalGenerati
 # To Do: Must add recorded audio
 SOURCE = None
 
-model = Speech2TextForConditionalGeneration.from_pretrained("facebook/s2t-small-librispeech-asr")
+model = Speech2TextForConditionalGeneration.from_pretrained(
+    "facebook/s2t-small-librispeech-asr"
+)
 processor = Speech2TextProcessor.from_pretrained("facebook/s2t-small-librispeech-asr")
 
 # add
 inputs = processor(SOURCE, sampling_rate=48000, return_tensors="pt")
 generated_ids = model.generate(
-    inputs["input_features"],
-    attention_mask=inputs["attention_mask"],
+    inputs["input_features"], attention_mask=inputs["attention_mask"]
 )
 
 transcription = processor.batch_decode(generated_ids, skip_special_tokens=True)
