68f9f9666cfc869285c058f7a26d05c7cd37f88b
DoodyShark <da2863@nyu.edu>
2024-04-11 00:14:31 -0400

    testing webapp

--- web-app/prompt.py
+++ web-app/prompt.py
@@ -28,44 +28,23 @@ class Prompt:
         self.prompt = prompt
         self.difficulty = difficulty
         self.number_of_attempts = number_of_attempts
-        if idef:
-            self.idef = idef
-        else:
-            Prompt.prompts.insert_one(self.to_bson())
-
-    def update_db(self):
-        """
-        update database
-        """
-        Prompt.prompts.replace_one({"_id": self.idef}, self.to_bson())
-
-    def get_id(self):
-        """
-        get id
-        """
-        return str(self.idef)
+        self.idef = idef
+        if not idef:
+            just_inserted = Prompt.prompts.insert_one(self.to_bson())
+            self.idef = just_inserted.inserted_id
 
     def to_bson(self) -> Dict:
         """
         convert object to bson
         """
         bson_dict = {}
-        if hasattr(self, "id"):
+        if self.idef:
             bson_dict["_id"] = self.idef
-            bson_dict["input"] = self.input
-            bson_dict["solution"] = self.solution
-            bson_dict["cookie"] = self.cookie
+        bson_dict["prompt"] = self.prompt
         return bson_dict
 
-    def from_bson(self, bson_dict: Dict) -> Prompt:
+    def get_prompt(self):
         """
-        initialize and return object from bson
+        prompt getter function
         """
-        if not bson_dict:
-            return None
-        return Prompt(
-            prompt=bson_dict["prompt"],
-            difficulty=bson_dict["difficulty"],
-            number_of_attempts=bson_dict["number_of_attempts"],
-            idef=bson_dict["_id"],
-        )
+        return self.prompt
--- web-app/tests/test_flask_webapp.py
+++ web-app/tests/test_flask_webapp.py
@@ -3,7 +3,7 @@
 """
 
 import pytest
-from app import create_app
+from app import create_app, Prompt, Transcription, Scoring, end_mgd
 
 
 class Tests:
@@ -11,40 +11,77 @@ class Tests:
     Class for handling tests
     """
 
-    @pytest.fixture()
+    @pytest.fixture
     def app(self):
         """
         Creates an app
         """
+
         app = create_app()
+
         app.config.update(
             {
                 "TESTING": True,
             }
         )
 
+        assert app.connected
+
         # other setup can go here
 
         yield app
 
         # clean up / reset resources here
+        if not hasattr(app, "db"):
+            app.db = None
+        if not hasattr(app, "se4_db"):
+            app.se4_db = None
+        end_mgd(app.db, app.se4_db)
 
-    @pytest.fixture()
-    def client(self, app):
+    # @pytest.fixture()
+    # def client(self, app):
+    #     """
+    #     Copied
+    #     """
+    #     return app.test_client()
+
+    # @pytest.fixture()
+    # def runner(self, app):
+    #     """
+    #     Copied
+    #     """
+    #     return app.test_cli_runner()
+
+    def test_prompt(self, app):
         """
         Copied
         """
-        return app.test_client()
 
-    @pytest.fixture()
-    def runner(self, app):
+        print(app)
+
+        p = Prompt("ABC", 5, 4)
+
+        found = Prompt.prompts.find_one({"_id": p.idef, "prompt": "ABC"})
+
+        for f in found:
+            print(f)
+
+        assert found
+
+    def test_transcript(self, app):
         """
         Copied
         """
-        return app.test_cli_runner()
 
-    def test_test(self):
-        """
-        Copied
-        """
-        assert True
+        print(app)
+
+        t = Transcription("test", "taste", Scoring("", 0))
+
+        found = Transcription.transcriptions.find_one(
+            {"_id": t.idef, "inputed": "test", "score": 0}
+        )
+
+        for f in found:
+            print(f)
+
+        assert found
