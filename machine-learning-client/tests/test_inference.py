d1383a2985ccb86128786b61ba5891bbe1f243b3
FirasBDarwish <94559551+FirasBDarwish@users.noreply.github.com>
2024-04-11 04:07:46 +0000

    fixed linting errors

--- machine-learning-client/tests/test_inference.py
+++ machine-learning-client/tests/test_inference.py
@@ -26,7 +26,10 @@ def test_inference():
     test whether speech2text returns proper string
     """
     dso = load_dataset(
-        "hf-internal-testing/librispeech_asr_demo", "clean", split="validation", trust_remote_code=True
+        "hf-internal-testing/librispeech_asr_demo",
+        "clean",
+        split="validation",
+        trust_remote_code=True
     )
     returned = speech2textpipeline(dso[0]["audio"]["array"])
     assert returned == [
@@ -53,22 +56,28 @@ def test_none_input():
 
 # test with multiple audio lengths
 def test_multiple_audio_lengths():
-    
-    # using hugging face internal testing dataset to generate audio samples (both long and short) of about 5 seconds and 30 seconds
-    short_audio = load_dataset("hf-internal-testing/librispeech_asr_demo", "clean", split="validation[0:1]", trust_remote_code=True)
-    long_audio = load_dataset("hf-internal-testing/librispeech_asr_demo", "clean", split="validation[5:6]", trust_remote_code=True)
-    
+    """
+    using hugging face internal testing dataset to generate audio samples
+    (both long and short) of about 5 seconds and 30 seconds
+    """
+    short_audio = load_dataset("hf-internal-testing/librispeech_asr_demo",
+                                "clean",
+                                split="validation[0:1]",
+                                trust_remote_code=True)
+    long_audio = load_dataset("hf-internal-testing/librispeech_asr_demo",
+                                "clean",
+                                split="validation[5:6]",
+                                trust_remote_code=True)
+
     # transcribe the audio samples using the speech2textpipeline from inference.py
     transcription_short = speech2textpipeline(short_audio[0]["audio"]["array"])
     transcription_long = speech2textpipeline(long_audio[0]["audio"]["array"])
 
-    # check if the transcription of the short audio is shorter than the transcription of the long audio
+    # check if the transcription of the short audio
+    # is shorter than the transcription of the long audio
     assert isinstance(transcription_short, list)
     assert isinstance(transcription_long, list)
     assert len(transcription_short[0]) < len(transcription_long[0])
-
-
-
 
 # class Tests:
 #     """Class defines tests"""
